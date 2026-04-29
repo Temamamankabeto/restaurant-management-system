d5316d2d4799455db37ed8e39398ca1773d2a92e
Temam <temamaman156@gmail.com>
2026-04-29 15:22:19 +0300

    Add centralized types exports

---
+++ frontend/types/index.ts
@@ -0,0 +1,4 @@
+export * from "@/types/shift-management/shift.type";
+export * from "@/types/payment-management/payment.type";
+export * from "@/types/report-management/report.type";
+export * from "@/types/notification-management/notification.type";
