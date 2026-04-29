f0b424fd43793a9f6f633f82893b11477d24b3a2
Temam <temamaman156@gmail.com>
2026-04-29 18:05:37 +0300

    Export role and permission hooks from hooks index

--- frontend/hooks/index.ts
+++ frontend/hooks/index.ts
@@ -1,4 +1,6 @@
 export * from "@/hooks/user-management/use-users";
+export * from "@/hooks/user-management/use-roles";
+export * from "@/hooks/user-management/use-permissions";
 export * from "@/hooks/shift-management/use-shifts";
 export * from "@/hooks/payment-management/use-payments";
 export * from "@/hooks/report-management/use-reports";
