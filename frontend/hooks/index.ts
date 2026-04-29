5de7488df6cbb0e08a91badc14a463e7f5ed3b23
Temam <temamaman156@gmail.com>
2026-04-29 15:21:50 +0300

    Add centralized hooks exports

---
+++ frontend/hooks/index.ts
@@ -0,0 +1,5 @@
+export * from "@/hooks/user-management/use-users";
+export * from "@/hooks/shift-management/use-shifts";
+export * from "@/hooks/payment-management/use-payments";
+export * from "@/hooks/report-management/use-reports";
+export * from "@/hooks/notification-management/use-notifications";
