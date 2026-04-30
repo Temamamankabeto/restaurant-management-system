49ecc3a475fb045f7a9d55f1f250fff6043fe07c
Temam <temamaman156@gmail.com>
2026-04-30 10:50:41 +0300

    Expose stock keeper inventory endpoints

--- backend/routes/stockkeeper.php
+++ backend/routes/stockkeeper.php
@@ -1,10 +1,20 @@
 <?php
 
 use App\Http\Controllers\Api\InventoryBatchController;
+use App\Http\Controllers\Api\InventoryItemController;
+use App\Http\Controllers\Api\InventoryTransactionController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])
     ->prefix('stock-keeper')
     ->group(function () {
+        Route::get('/inventory/items', [InventoryItemController::class, 'index']);
+        Route::get('/inventory/items/{id}', [InventoryItemController::class, 'show']);
+
+        Route::get('/inventory/transactions', [InventoryTransactionController::class, 'index']);
+        Route::post('/inventory/items/{id}/adjust', [InventoryTransactionController::class, 'adjust']);
+        Route::post('/inventory/items/{id}/waste', [InventoryTransactionController::class, 'waste']);
+        Route::post('/inventory/items/{id}/transfer', [InventoryTransactionController::class, 'transfer']);
+
         Route::get('/inventory/batches', [InventoryBatchController::class, 'index']);
     });
