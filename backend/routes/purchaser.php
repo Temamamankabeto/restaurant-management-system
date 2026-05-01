e018ddd08d72a881b797c75c9badd9e87223d9d4
Temam <temamaman156@gmail.com>
2026-05-01 10:38:14 +0300

    Expose purchaser lookup routes for purchase orders

--- backend/routes/purchaser.php
+++ backend/routes/purchaser.php
@@ -1,19 +1,22 @@
 <?php
 
+use App\Http\Controllers\Api\InventoryItemController;
 use App\Http\Controllers\Api\PurchaseOrderController;
+use App\Http\Controllers\Api\SupplierController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])
     ->prefix('purchaser')
     ->group(function () {
+        Route::get('/suppliers', [SupplierController::class, 'index']);
+        Route::get('/suppliers/{id}', [SupplierController::class, 'show']);
+
+        Route::get('/inventory/items', [InventoryItemController::class, 'index']);
+        Route::get('/inventory/items/{id}', [InventoryItemController::class, 'show']);
 
         Route::get('/purchase-orders', [PurchaseOrderController::class, 'index']);
-
         Route::get('/purchase-orders/{id}', [PurchaseOrderController::class, 'show']);
-
         Route::get('/purchase-orders/{id}/history', [PurchaseOrderController::class, 'history']);
-
         Route::post('/purchase-orders', [PurchaseOrderController::class, 'store']);
-
         Route::post('/purchase-orders/{id}/submit', [PurchaseOrderController::class, 'submit']);
     });
