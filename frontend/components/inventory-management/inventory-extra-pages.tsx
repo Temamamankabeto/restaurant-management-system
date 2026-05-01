a59ac42798be87ae3429d6c4b264b4a24ecda10a
Temam <temamaman156@gmail.com>
2026-05-01 09:58:46 +0300

    Add inventory extra pages entry file

---
+++ frontend/components/inventory-management/inventory-extra-pages.tsx
@@ -0,0 +1,6 @@
+"use client";
+
+// Additional inventory UI pages: transfer, trashed items, reorder suggestions, expired items, receiving history
+// Imported by route-level pages under /dashboard/inventory/*
+
+export * from "./inventory-extra-pages.impl";
