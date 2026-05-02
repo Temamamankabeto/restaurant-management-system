62087c7ffa24999a6cf7838b19077f173ae86be4
Temam <temamaman156@gmail.com>
2026-05-02 12:52:08 +0300

    Support PIN fields on credit account users

--- backend/app/Models/CreditAccountUser.php
+++ backend/app/Models/CreditAccountUser.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 
 class CreditAccountUser extends Model
@@ -15,15 +16,33 @@ class CreditAccountUser extends Model
         'id_number',
         'daily_limit',
         'monthly_limit',
+        'pin_hash',
+        'pin_enabled',
         'is_active',
         'created_by',
+    ];
+
+    protected $hidden = [
+        'pin_hash',
+    ];
+
+    protected $appends = [
+        'has_pin',
     ];
 
     protected $casts = [
         'daily_limit' => 'decimal:2',
         'monthly_limit' => 'decimal:2',
+        'pin_enabled' => 'boolean',
         'is_active' => 'boolean',
     ];
+
+    protected function hasPin(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => !empty($this->pin_hash),
+        );
+    }
 
     public function account()
     {
